6d7639e4baf15f655eac9557cdca0ea64c9db39d
Alex Smirnov <97alexsmirnov@gmail.com>
2019-05-22 05:28:40 -0700

    Initial commit.

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+	import {HistoryRouter, RouterProvider, RouterLink, RouterView} from '@svel/router';
+
+	import AboutMe from './pages/AboutMe.svelte';
+
+	// const router = new HistoryRouter({
+	// 	base: __dirname,
+	// 	routes: [
+	// 		{path: '/', component: AboutMe}
+	// 	]
+	// });
+	//
+	// let n = 0;
+	// let route = router.currentRoute;
+	// router.listen(r => route = r)
+	//
+	// function navigateAndIncrement () {
+	// 	const increment = () => n++
+	// 	route = router.currentRoute;
+	// 	if (router.currentRoute.path === '/') {
+	// 		router.push('/foo', increment)
+	// 	} else {
+	// 		router.push('/', increment)
+	// 	}
+	// }
+
+</script>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
+
+<h1>Hello.</h1>
+
+<!--<RouterProvider {router}>-->
+<!--	<div id="app">-->
+<!--		<h1>Basic</h1>-->
+<!--		<ul>-->
+<!--			<li><RouterLink to="/">/</RouterLink></li>-->
+<!--		</ul>-->
+<!--	</div>-->
+<!--</RouterProvider>-->
