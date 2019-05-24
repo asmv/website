735c8bc0afe5efd4a9ecc029984c10b8f566208b
Alex Smirnov <97alexsmirnov@gmail.com>
2019-05-24 02:27:05 -0700

    Looks like this file was not committed.

---
+++ src/Layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import NavBar from "./components/Navbar.svelte"
+
+    export let iComponent;
+    export let reqData;
+    export let pageRoutes = {};
+</script>
+
+<style>
+    footer{
+        background-color: darkturquoise;
+    }
+</style>
+
+<NavBar {pageRoutes} {reqData}></NavBar>
+<svelte:component this={iComponent} {reqData}/>
+<footer>
+    <h3>Footer in Layout </h3>
+</footer>
