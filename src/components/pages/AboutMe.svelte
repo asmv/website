7b6e301cb5034ddb767a1a7730ad9c97fed44b08
Alex Smirnov <97alexsmirnov@gmail.com>
2019-05-24 23:12:33 -0700

    Added description to index page.

--- src/components/pages/AboutMe.svelte
+++ src/components/pages/AboutMe.svelte
@@ -8,9 +8,22 @@
 <div>
     <h1>Alexander Smirnov</h1>
     <div class="AboutMeIntro">
-        <p>Neque dolorem dolores ratione id. Et blanditiis aut debitis ab adipisci nihil. Aspernatur a rerum officiis quia similique id impedit. Enim est nobis et voluptatem. Qui placeat quae non rerum quia vero. Molestiae et pariatur velit rerum incidunt nemo dignissimos.</p>
-        <p>Suscipit id inventore sed praesentium dignissimos architecto et. Exercitationem labore est saepe. Dignissimos culpa esse nesciunt non. Et vitae rerum odio in labore.</p>
-        <p>Dolor molestias veritatis voluptas dolorem non. Voluptatem harum nisi aut rerum et repellendus autem. Sed similique aut assumenda consequatur veritatis placeat. Expedita vel facilis omnis. Explicabo fugiat qui harum iure quaerat. Aut delectus qui ratione iure magni vel doloribus sed.</p>
+        <p>Hello and welcome to my humble website!</p>
+        <p>I am Alexander Smirnov:</p>
+        <ol>
+            <li>Game Developer</li>
+            <li>Hobby Programmer</li>
+            <li>Student at UC Santa Cruz</li>
+        </ol>
+        <p>There isn't too much content here now, but I will be adding more pages and projects as I keep developing the website.</p>
+        <p>You can find the source code for this site on my github page.</p>
     </div>
-<!--    Container Icon Link Here -->
+    <div>
+        <!-- Container Icon Link Here -->
+        <h4>A few links that may be of interest:</h4>
+        <ul>
+            <li><a href="https://www.linkedin.com/in/97alex-smirnov/">Linked-In</a></li>
+            <li><a href="https://github.com/asmv">Github</a></li>
+        </ul>
+    </div>
 </div>
